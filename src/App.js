09d01065a16476f741e92ff260dcd7d68f389c44
salonidhatrak <saloni123@gmail.com>
2022-10-16 23:36:12 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
      <h1>Navbar</h1>
+     <Navbar/>
     </div>
   );
 }
